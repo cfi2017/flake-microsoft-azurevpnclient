a47603526d1587d033e5909159ee2f6923ac34f4
Carlo Field <carlo.field@adfinis.com>
2026-02-05 17:38:07 +0100

    fix: try and update
    Signed-off-by: Carlo Field <carlo.field@adfinis.com>

--- flake.nix
+++ flake.nix
@@ -22,17 +22,6 @@
             pname = "microsoft-azurevpnclient";
             version = "3.0.0";
 
-            passthru.wrapper = pkgs.writeShellScript "microsoft-azurevpnclient-wrapper" ''
-              export PATH="${pkgs.openvpn}/bin:${pkgs.zenity}/bin:$PATH"
-              export LD_LIBRARY_PATH="${passthru.installPath}/lib:${nixpkgs.lib.makeLibraryPath buildInputs}:$LD_LIBRARY_PATH"
-              exec ${pkgs.bubblewrap}/bin/bwrap \
-                --dev-bind / / \
-                --bind ${renamedCerts}/ssl/certs /etc/ssl/certs \
-                /run/wrappers/bin/microsoft-azurevpnclient-wrapped "$@"
-            '';
-
-            passthru.installPath = placeholder "out";
-
             src = pkgs.fetchurl {
               url = "https://packages.microsoft.com/ubuntu/22.04/prod/pool/main/m/microsoft-azurevpnclient/microsoft-azurevpnclient_${version}_amd64.deb";
               hash = "sha256-nl02BDPR03TZoQUbspplED6BynTr6qNRVdHw6fyUV3s=";
@@ -112,8 +101,15 @@
               ln -s $out/opt/microsoft/microsoft-azurevpnclient/microsoft-azurevpnclient $out/bin/microsoft-azurevpnclient
               ln -s $out/opt/microsoft/microsoft-azurevpnclient/lib $out
 
-              cp ${passthru.wrapper} $out/bin/microsoft-azurevpnclient
-              chmod +x $out/bin/microsoft-azurevpnclient
+              cp ${pkgs.writeShellScript "microsoft-azurevpnclient-wrapper" ''
+                export PATH="${pkgs.openvpn}/bin:${pkgs.zenity}/bin:$PATH"
+                export LD_LIBRARY_PATH="$out/lib:${nixpkgs.lib.makeLibraryPath buildInputs}:$LD_LIBRARY_PATH"
+                exec ${pkgs.bubblewrap}/bin/bwrap \
+                  --dev-bind / / \
+                  --bind ${renamedCerts}/ssl/certs /etc/ssl/certs \
+                  $out/opt/microsoft/microsoft-azurevpnclient/microsoft-azurevpnclient "$@"
+              ''} $out/bin/microsoft-azurevpnclient-inner
+              chmod +x $out/bin/microsoft-azurevpnclient-inner
 
               # TODO:
               # Fix desktop file location
@@ -145,7 +141,7 @@
         owner = "root";
         group = "root";
         capabilities = "cap_net_admin+eip";
-        source = "${self.packages.${pkgs.system}.default}/opt/microsoft/microsoft-azurevpnclient/microsoft-azurevpnclient";
+        source = "${self.packages.${pkgs.system}.default}/bin/microsoft-azurevpnclient-inner";
       };
     };
   };
